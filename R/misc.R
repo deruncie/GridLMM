9c16775afe3ecce6ffb33f987bddff71cc9dd518
Dan Runcie <deruncie@ucdavis.edu>
2021-03-17 12:22:16 -0700

    docs

--- R/misc.R
+++ R/misc.R
@@ -1,13 +1,11 @@
 #' Detect number of available cores on system
 #'
-#' \code{my_detectCores()} detects the numer of cores available for calls to \code{\link{mclapply}}
+#' \code{my_detectCores()} detects the number of cores available for calls to \code{\link{mclapply}}
 #' or \code{\link{registerDoParallel}}.
 #' 
-#' The command is specific for clusters running slurm. 
-#' Accesses the \code{SLURM_CPUS_PER_TASK} environmental variable. If this
-#' variable doesn't exist it uses \code{\link[parallel]{detectCores}}.
+#' This uses RcppParallel::defaultNumThreads() which seems to generally work for me.
 #' 
-#' @return ncores integer of number of avaialble cores
+#' @return ncores integer of number of available cores
 #'
 #' @export
 #'
